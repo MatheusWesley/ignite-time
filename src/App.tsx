8d441a26a2e4e92688a8c090ff8a9e84a3cd50e5
MatheusWesley <matheus.tarra@gmail.com>
2025-03-12 15:13:27 -0300

    Ajuste no App.tsx para usar hash

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Router } from "./Router";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CyclesContextProvider } from "./contexts/CyclesContext";
 import { ToastContainer } from "react-toastify";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <HashRouter>
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
         <GlobalStyle />
         <ToastContainer />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   )
 }
